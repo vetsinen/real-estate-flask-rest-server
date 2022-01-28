59554c207a92a79de3bd8c5eb4b46cd44ce91ffe
vetsinen <vetsinen@gmail.com>
2022-01-29 00:32:20 +0200

    cleared

--- app.py
+++ app.py
@@ -6,28 +6,27 @@ import json
 with open('db.json') as json_file:
     dbrooms = json.load(json_file)
 
-
 MAX_ITEMS = 15
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+
 @app.route("/districts")
 @cross_origin()
 def districts():
     return jsonify(["pechersky", "svytoshinsky", "desnyansky"])
 
+
 @app.route("/rooms/<string:id>")
 @cross_origin()
 def rooms_by_district(id):
     filtered = [x for x in dbrooms if x['district'] == id]
-    print(len(filtered))
     return jsonify(filtered[:MAX_ITEMS])
+
 
 @app.route("/rooms")
 @cross_origin()
 def rooms():
-    print(type(dbrooms))
     return jsonify(dbrooms[:MAX_ITEMS])
-
